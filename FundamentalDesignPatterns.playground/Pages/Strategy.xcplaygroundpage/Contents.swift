d271730137af14e338dadbea812fd5daf7c60544
AdPiscinam <lab@walimaloui.com>
2022-08-01 20:37:55 +0200

    Implement Strategy Pattern

--- FundamentalDesignPatterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
@@ -11,3 +11,53 @@
  
  ## Code Example
  */
+import UIKit
+
+protocol MovieRatingStrategy {
+    var ratingServiceName: String {get}
+    
+    func fetchRating(for movieTitle: String,
+                     success: (_ rating: String, _ review: String) -> ())
+}
+
+class RottenTomatoesClient: MovieRatingStrategy {
+    var ratingServiceName =  "Rotten Tomatoes"
+    
+    func fetchRating(for movieTitle: String, success: (String, String) -> ()) {
+        let rating = "95%"
+        let review = "It rocked!"
+        success(rating, review)
+    }
+}
+
+class IMDbClient: MovieRatingStrategy {
+    var ratingServiceName = "IMDb"
+    
+    func fetchRating(for movieTitle: String, success: (String, String) -> ()) {
+        let rating = "3/10"
+        let review = "It was terrible! The audience was throwing rotten Tomatoes"
+        success(rating, review)
+    }
+}
+
+class MovieRatingViewController: UIViewController {
+    var movieRatingClient: MovieRatingStrategy!
+    
+    @IBOutlet var movieTitleTextField: UITextField!
+    @IBOutlet var ratingServiceNameLabel: UILabel!
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var reviewLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ratingServiceNameLabel.text = movieRatingClient.ratingServiceName
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        guard let movieTitle = movieTitleTextField.text, movieTitle.count > 0 else { return }
+        movieRatingClient.fetchRating(for: movieTitle) { rating, review in
+            self.ratingLabel.text = rating
+            self.reviewLabel.text = review
+        }
+    }
+}
