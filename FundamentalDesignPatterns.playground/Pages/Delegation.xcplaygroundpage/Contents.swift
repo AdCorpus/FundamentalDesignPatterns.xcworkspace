a4a5c700478a5d44d50b3ea23c89e985cae55fe7
AdPiscinam <lab@walimaloui.com>
2022-08-01 16:18:55 +0200

    Implement the 'Delegation' part

--- FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
@@ -11,3 +11,47 @@
  
  ## Code Example
  */
+import UIKit
+
+protocol MenuViewControllerDelegate: AnyObject {
+    func menuViewController(_ menuViewController: MenuViewController,
+                            didSelectItemAtIndex index: Int)
+}
+
+class MenuViewController: UIViewController {
+    
+    weak var delegate: MenuViewControllerDelegate?
+    
+    @IBOutlet var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+     
+    private let items = ["item 1","item 2","item 3"]
+}
+
+extension MenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+}
+
+extension MenuViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        delegate?.menuViewController(self, didSelectItemAtIndex: indexPath.row)
+    }
+}
